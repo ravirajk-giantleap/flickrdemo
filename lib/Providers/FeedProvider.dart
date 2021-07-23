61d1e34fe04598a9ec26177ab68f07fd7cdd118e
ravirajk-giantleap <raviraj.kundekar@giantleapsystems.com>
2021-07-23 14:46:42 +0530

    Added  Right Swipe to delete change

--- lib/Providers/FeedProvider.dart
+++ lib/Providers/FeedProvider.dart
@@ -1,15 +1,25 @@
+import 'dart:convert';
+
 import 'package:flickrdemo/Models/Feedmodel.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 class FeedProvider extends ChangeNotifier{
 
-  List<FeedModel> _feedModel=[];
-  List<FeedModel> get data=>_feedModel;
+  List<FeedModel> _feedList=[];
+  List<FeedModel> get data=>_feedList;
 
   Future<Null> getFeed(context)async{
-    final raw = await DefaultAssetBundle.of(context).loadString('local.json');
-    _feedModel =  feedModelFromJson(raw);
-    notifyListeners();
+   final raw = await rootBundle.loadString('assets/post.json');
+   final data = json.decode(raw);
+   var ndata = data as List;
+   _feedList = ndata.map((e) => FeedModel.fromJson(e)).toList();
+   notifyListeners();
+   return null;
   }
 
+  remove(int index){
+    _feedList.removeAt(index);
+    notifyListeners();
+  }
 }
--- lib/Ui/Imageshome.dart
+++ lib/Ui/Imageshome.dart
@@ -1,4 +1,5 @@
 
+import 'package:flickrdemo/Providers/FeedProvider.dart';
 import 'package:flickrdemo/Providers/ImageProvider.dart';
 import 'package:flickrdemo/Ui/DetailsView.dart';
 import 'package:flutter/material.dart';
@@ -18,77 +19,131 @@ class _ImagesHomeState extends State<ImagesHome> {
     Future.delayed(
       Duration.zero,(){
         context.read<Imageprovider>().getImages(mounted);
+        context.read<FeedProvider>().getFeed(context);
       }
     );
   }
   @override
   Widget build(BuildContext context) {
     final imageData= context.watch<Imageprovider>();
+    final feedData = context.watch<FeedProvider>();
     return Scaffold(
       appBar: AppBar(
         title: Text('Images'),
       ),
        body: RefreshIndicator(
-      child: ListView(
-        children: [
-        Container(
-        height: 300,
-        child: ListView.builder(
-          shrinkWrap: true,
-          primary: true,
-          //physics: ScrollPhysics(),
-          scrollDirection: Axis.horizontal,
-          itemCount: imageData.data.length,
-          itemBuilder: (context ,i){
-            return Container(
-              height: 300,
-              margin: EdgeInsets.all(5),
-              padding: EdgeInsets.all(7),
-              child: GestureDetector(
-                  onTap: ()=>Navigator.push(context, MaterialPageRoute(
-                  fullscreenDialog: true,
-                  builder: 
-                  (context)=>DetailsView(item: imageData.data[i],))),
-                  child: Column(
+          child: Column(
                   children: [
-                   Container(
-                        height: 200,
-                        width: 250,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          image: DecorationImage(
-                            image: NetworkImage(imageData.data[i].media!.m!),
-                            fit: BoxFit.cover
-                          )
+                  Expanded(flex: 4,
+                  child: ListView.builder(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: imageData.data.length,
+                  itemBuilder: (context ,i){
+                    return Container(
+                      height: 300,
+                      margin: EdgeInsets.all(5),
+                      padding: EdgeInsets.all(7),
+                      child: GestureDetector(
+                          onTap: ()=>Navigator.push(context, MaterialPageRoute(
+                          fullscreenDialog: true,
+                          builder: 
+                          (context)=>DetailsView(item: imageData.data[i],))),
+                          child: Column(
+                          children: [
+                           Container(
+                                height: 200,
+                                width: 250,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  image: DecorationImage(
+                                    image: NetworkImage(imageData.data[i].media!.m!),
+                                    fit: BoxFit.cover
+                                  )
+                                ),
+                              ),
+                            Text(imageData.data[i].title!),
+                            imageData.data[i].ratings!=null? 
+                            Text("Rated:-\t"+imageData.data[i].ratings.toString()
+                            +'\tprovided by:'+imageData.data[i].name.toString())
+                            :Container()
+                          ],
                         ),
                       ),
-                    Text(imageData.data[i].title!),
-                    imageData.data[i].ratings!=null? 
-                    Text("Rated:-\t"+imageData.data[i].ratings.toString()
-                    +'\tprovided by:'+imageData.data[i].name.toString())
-                    :Container()
+                    );
+                  },
+          ),),
+          Expanded(
+          flex: 7,
+          child: ListView.builder(
+            shrinkWrap: true,
+            scrollDirection: Axis.vertical,
+            physics: ScrollPhysics(),
+            itemCount: feedData.data.length,
+            itemBuilder: (context,index){
+              final item = feedData.data[index];
+              return Dismissible(
+                  key: UniqueKey(),
+                  direction: DismissDirection.startToEnd,
+                  onDismissed: (direction){
+                    if(direction == DismissDirection.startToEnd){
+                      context.read<FeedProvider>().remove(index);
+                    }
+                  },
+                  child: Container(
+                  padding: EdgeInsets.only(top:7),
+                  margin: EdgeInsets.only(top:10,right: 15,left: 15),
+                  height: 350,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 8,
+                        color: Colors.black26
+                      )
+                    ]
+                  ),
+                child: Column(
+                  children: [
+                   Row(
+                     children: [
+                        CircleAvatar(
+                          backgroundImage: NetworkImage(item.profile!),
+                        ),
+                        SizedBox(width: 10,),
+                        Text(item.name!,
+                        style: TextStyle(fontSize: 18,fontWeight: FontWeight.w400),
+                        )
+                     ],
+                   ),
+                   SizedBox(height: 5,),
+                   Text(item.comment!,
+                   style: TextStyle(fontSize: 16,fontWeight: FontWeight.w300,
+                   ),
+                   textAlign: TextAlign.left,
+                   ),
+                   SizedBox(height: 10,),
+                   Container(
+                     height: 180,
+                     width: 300,
+                     child: Image.network(item.commentphoto!,
+                     fit: BoxFit.cover,
+                     ),
+                   )
                   ],
                 ),
-              ),
-            );
-          },
-      ),
-        ),
-      SizedBox(
-        height: 10,
-      ),
-      ListView.builder(
-        itemBuilder: (BuildContext context,int index){
-          return Container();
-        },
-      )
-
-      ],
-      ),
-       
-      onRefresh: ()async{
+                ),
+              );
+            },
+          )
+          
+          )
+          ],
+          ),
+        onRefresh: ()async{
         context.read<Imageprovider>().onRefresh(mounted);
-      }),
+      }
+      ),
     );
   }
 }
